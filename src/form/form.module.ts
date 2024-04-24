57cdf49d472d599494a216189e21927c1174f37a
Mehedi-ICE16 <mehedi.ice.16@gmail.com>
2024-04-24 21:07:23 +0600

    added all file of form

--- src/form/form.module.ts
+++ src/form/form.module.ts
@@ -1,7 +1,34 @@
-import { Form } from './form.controller';
-import { TeamService } from './team.service';
-import { teamProvider } from './team.provider';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { FormController } from './form.controller';
+import { FormService } from './form.service';
+import { formProvider } from './form.provider';
 import { DatabaseModule } from '../database/database.module';
 import { JwtMiddleware } from '../auth/jwt.middleware';
 import { EmployeeLoginService } from '../employee_login/employee_login.service';
 import { employeeLoginProvider } from '../employee_login/employee_login.provider';
+
+
+@Module({
+    imports: [DatabaseModule],
+    controllers: [FormController],
+    providers: [
+      FormService,
+      formProvider,
+      // JwtMiddleware,
+      // EmployeeLoginService,
+      // employeeLoginProvider
+    ],
+  })
+
+
+  //  export class DepartmentModule implements NestModule {
+  //   configure(consumer: MiddlewareConsumer) {
+  //     consumer
+  //       .apply(JwtMiddleware).forRoutes(DepartmentController);
+  //   }
+  // }
+
+  export class FormModule { }
+
+  // ,JwtModule.register({ secret: 'my_secret_key', signOptions: { expiresIn: '12h' } })
